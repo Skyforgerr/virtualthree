6122ddcbf6e94d1ef6f955ea0d5c99a1cb130920
Skyforgerr <postnikoviivan@gmail.com>
2023-10-01 20:31:04 +0300

    теперь нет ошибки в тестах, но есть с плагином mvn и target/classes

--- third/src/main/java/com/virtual/third/controller/ElementController.java
+++ third/src/main/java/com/virtual/third/controller/ElementController.java
@@ -1,12 +1,20 @@
 package com.virtual.third.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+
+import java.nio.file.Path;
 
 import com.virtual.third.model.Element;
 import com.virtual.third.repository.ElementRepository;
@@ -27,6 +35,28 @@ public class ElementController {
     @GetMapping("/viewelements")
     public List<Element> viewElements() {
         return repository.findAll();
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadFile() throws IOException {
+        // Путь к файлу внутри контейнера
+        Path filePath = Paths.get("/app/MIREA_Gerb_Colour.png");
+
+        try {
+            // Создание ресурса из файла
+            Resource resource = new UrlResource(filePath.toUri());
+
+            // Проверка, существует ли файл
+            if (resource.exists()) {
+                return ResponseEntity.ok()
+                        .header("Content-Disposition", "attachment; filename=\"MIREA_Gerb_Colour.png\"")
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("File not found.", e);
+        }
     }
 
 }
